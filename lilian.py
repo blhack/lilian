56d47ba86cafe61c2529f835fe073888fd29e3a8
Ryan <blhack@gmail.com>
2013-06-02 01:55:42 +0000

    Added a logging function

--- lilian.py
+++ lilian.py
@@ -5,6 +5,7 @@ import time
 import os
 import sys
 import warnings
+import datetime
 
 #see parms.py for parameters
 
@@ -30,18 +31,23 @@ now = time.time()
 #meat and potatoes -- work gets done below here
 
 def log(message):
-	open(logfile,"a")
+	today = datetime.datetime.today()
+	log_file = open(logfile,"a")
+	log_file.write("[%s/%s/%s - %s:%s:%s] %s \n" % (today.month,today.day,today.year,today.hour,today.minute,today.second,message))
 
 
 def auth(user,password):
 	c.execute("select password from users where user = %s", (user))
 	results = c.fetchall()
 	if len(results) == 0:
+		log("Did not authenticate user %s.  I do not know who this is." % (user))
 		return(0)
 	hashed = results[0][0]
 	if bcrypt.hashpw(password, hashed) == hashed:
+		log("Authenticated %s" % (user))
 		return(1)
 	else:
+		log("Did not authenticate %s.  Invalid password." % (user))
 		return(0)
 		
 
@@ -51,5 +57,6 @@ def register(user,password):
 		c.execute("insert into users (user,password,registration_date) values(%s,%s,%s)", (user,hashed,now))
 	except MySQLdb.IntegrityError:
 		return("dupe")
-		
 	return(1)
+
+
